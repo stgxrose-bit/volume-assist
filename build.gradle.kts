51b11b57b235fb407f3e7f05ae217c721c81b7ba
stgxrose-bit <stgxrose@gmail.com>
2026-09-01 17:00:09 +0530

    Upgrade AGP to 8.5.0 and Kotlin to 1.9.24 for API 36 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
